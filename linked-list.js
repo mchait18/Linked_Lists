3eac8eb3178bda12142a7db2bdcc45b0c88b1cb7
mchait <miriamchait@gmail.com>
2023-09-26 13:20:28 -0400

    further study

--- linked-list.js
+++ linked-list.js
@@ -18,7 +18,16 @@ class LinkedList {
     for (let val of vals) this.push(val);
   }
 
-
+  /**returns node at indx passed in */
+  _get(idx) {
+    let count = 0;
+    let currentVal = this.head
+    while (currentVal) {
+      if (idx === count) return currentVal
+      count++
+      currentVal = currentVal.next
+    }
+  }
 
   /** push(val): add new value to end of list. */
 
@@ -58,16 +67,6 @@ class LinkedList {
 
     return this.removeAt(0)
 
-  }
-  /**returns node at indx passed in */
-  _get(idx) {
-    let count = 0;
-    let currentVal = this.head
-    while (currentVal) {
-      if (idx === count) return currentVal
-      count++
-      currentVal = currentVal.next
-    }
   }
 
   /** getAt(idx): get val at idx. */
@@ -146,7 +145,17 @@ class LinkedList {
     return sum / this.length
   }
 }
-/** LinkedList: chained together nodes. */
+/** Node: node for a doubly linked list. */
+class DoubleNode {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.previous = null;
+  }
+}
+
+
+/** DoublyLinkedList: chained together nodes. */
 
 class DoublyLinkedList {
   constructor(vals = []) {
@@ -155,6 +164,140 @@ class DoublyLinkedList {
     this.length = 0;
 
     for (let val of vals) this.push(val);
+  }
+  /**returns node at indx passed in */
+  _get(idx) {
+    let count = 0;
+    let currentVal = this.head
+    while (currentVal) {
+      if (idx === count) return currentVal
+      count++
+      currentVal = currentVal.next
+    }
+  }
+
+  /** push(val): add new value to end of list. */
+
+  push(val) {
+    let newNode = new DoubleNode(val);
+    /* empty list */
+    if (!this.head) {
+      this.head = newNode
+      this.tail = newNode
+    }
+    else {
+      this.tail.next = newNode
+      newNode.previous = this.tail
+      this.tail = newNode
+    }
+    this.length++;
+  }
+
+  /** unshift(val): add new value to start of list. */
+
+  unshift(val) {
+    let newNode = new DoubleNode(val);
+    /** if not empty list */
+    if (this.head !== null) {
+      newNode.next = this.head
+      this.head.previous = newNode
+    } else /* empty list*/ this.tail = newNode
+    this.head = newNode
+    this.length++;
+  }
+
+  /** pop(): return & remove last item. */
+
+  pop() {
+    let lastNode = this.tail
+    this.tail = lastNode.previous
+    this.tail.next = null
+    return lastNode.val
+    // return this.removeAt(this.length - 1)
+  }
+
+  /** shift(): return & remove first item. */
+
+  shift() {
+
+    return this.removeAt(0)
+
+  }
+
+  /** getAt(idx): get val at idx. */
+
+  getAt(idx) {
+    if (idx >= this.length || idx < 0)
+      throw new Error("Invalid index.");
+
+    return this._get(idx).val
+  }
+
+  /** setAt(idx, val): set val at idx to val */
+
+  setAt(idx, val) {
+    if (idx >= this.length || idx < 0)
+      throw new Error("Invalid index.");
+
+    this._get(idx).val = val
+
+  }
+
+  /** insertAt(idx, val): add node w/val before idx. */
+
+  insertAt(idx, val) {
+    if (idx > this.length || idx < 0)
+      throw new Error("Invalid index.");
+
+    if (idx === 0) return this.unshift(val)
+    if (idx === this.length) return this.push(val)
+
+    let newNode = new DoubleNode(val);
+    let curr = this._get(idx)
+    newNode.next = curr
+    curr.previous.next = newNode
+    this.length++;
+  }
+
+  /** removeAt(idx): return & remove item at idx, */
+
+  removeAt(idx) {
+    if (idx >= this.length || idx < 0 || this.length == 0)
+      throw new Error("Invalid index.");
+
+    let curr = this._get(idx)
+    /* if one item in list*/
+    if (this.length === 1) {
+      this.head = null
+      this.tail = null
+    }
+    /* if first item*/
+    else if (idx === 0) {
+      this.head = curr.next
+    }
+    else {
+      //let prev = this._get(idx - 1)
+      curr.previous.next = curr.next
+      /*if last item */
+      if (!curr.previous.next) {
+        this.tail = curr.previous
+      }
+    }
+    this.length--
+    return curr.val
+  }
+
+  /** average(): return an average of all values in the list */
+
+  average() {
+    if (this.length == 0) return 0
+    let sum = 0;
+    let currentVal = this.head
+    while (currentVal) {
+      sum += currentVal.val
+      currentVal = currentVal.next
+    }
+    return sum / this.length
   }
 }
 
--- linked-list.test.js
+++ linked-list.test.js
@@ -1,8 +1,8 @@
-const LinkedList = require("./linked-list");
+const DoublyLinkedList = require("./linked-list");
 
 describe("push", function () {
   it("appends node and increments length", function () {
-    let lst = new LinkedList();
+    let lst = new DoublyLinkedList();
 
     lst.push(5);
     expect(lst.length).toBe(1);
@@ -25,7 +25,7 @@ describe("push", function () {
 
 describe("unshift", function () {
   it("adds node at start and increments length", function () {
-    let lst = new LinkedList();
+    let lst = new DoublyLinkedList();
 
     lst.unshift(5);
     expect(lst.length).toBe(1);
@@ -48,7 +48,7 @@ describe("unshift", function () {
 
 describe("pop", function () {
   it("removes node at end and decrements length", function () {
-    let lst = new LinkedList([5, 10]);
+    let lst = new DoublyLinkedList([5, 10]);
 
     expect(lst.pop()).toBe(10);
     expect(lst.head.val).toBe(5);
@@ -64,7 +64,7 @@ describe("pop", function () {
 
 describe("shift", function () {
   it("removes node at start and decrements length", function () {
-    let lst = new LinkedList([5, 10]);
+    let lst = new DoublyLinkedList([5, 10]);
 
     expect(lst.shift()).toBe(5);
     expect(lst.tail.val).toBe(10);
@@ -79,7 +79,7 @@ describe("shift", function () {
 
 describe("getAt", function () {
   it("gets val at index", function () {
-    let lst = new LinkedList([5, 10]);
+    let lst = new DoublyLinkedList([5, 10]);
 
     expect(lst.getAt(0)).toBe(5);
     expect(lst.getAt(1)).toBe(10);
@@ -88,7 +88,7 @@ describe("getAt", function () {
 
 describe("setAt", function () {
   it("sets val at index", function () {
-    let lst = new LinkedList([5, 10]);
+    let lst = new DoublyLinkedList([5, 10]);
 
     expect(lst.setAt(0, 1));
     expect(lst.setAt(1, 2));
@@ -99,7 +99,7 @@ describe("setAt", function () {
 
 describe("insertAt", function () {
   it("inserts node and adjusts nearby nodes", function () {
-    let lst = new LinkedList([5, 10, 15, 20]);
+    let lst = new DoublyLinkedList([5, 10, 15, 20]);
 
     lst.insertAt(2, 12);
     expect(lst.length).toBe(5);
@@ -115,7 +115,7 @@ describe("insertAt", function () {
   });
 
   it("inserts into empty list", function () {
-    let lst = new LinkedList();
+    let lst = new DoublyLinkedList();
 
     lst.insertAt(0, 5);
     expect(lst.length).toBe(1);
@@ -126,7 +126,7 @@ describe("insertAt", function () {
 
 describe("removeAt", function () {
   it("removes from 1-item list", function () {
-    let lst = new LinkedList(["a"]);
+    let lst = new DoublyLinkedList(["a"]);
 
     lst.removeAt(0);
     expect(lst.length).toBe(0);
@@ -137,12 +137,12 @@ describe("removeAt", function () {
 
 describe("average", function () {
   it("calculates the average of items in a list", function () {
-    let lst = new LinkedList([2, 3, 1, 1, 7, 6, 9]);
+    let lst = new DoublyLinkedList([2, 3, 1, 1, 7, 6, 9]);
     expect(lst.average()).toBeCloseTo(4.1429, 4);
   });
 
   it("returns 0 for empty lists", function () {
-    let lst = new LinkedList();
+    let lst = new DoublyLinkedList();
     expect(lst.average()).toBe(0);
   });
 });
